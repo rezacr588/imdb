16a64179af45a75dc1f2cd68a6cd3d79a808f3ca
Reza Zeraat <rzeraat.tur@gmail.com>
2022-08-09 20:15:38 +0300

    add bottom header

--- components/General/HomeBottom/HomeBottom.tsx
+++ components/General/HomeBottom/HomeBottom.tsx
@@ -1,14 +1,35 @@
 /* eslint-disable @next/next/no-img-element */
+import { useRouter } from "next/router";
 import { FC } from "react";
 import { StyledContainer, Icon } from "./styled";
 
 export const HomeBottom: FC = () => {
+  const router = useRouter();
+
   return (
     <StyledContainer>
-      <Icon src="/movies-active.svg" alt="movies-active" />
-      <Icon src="/event-ticket-inactive.svg" alt="event-ticket-inactive" />
-      <Icon src="/alarm-inactive.svg" alt="alarm-inactive" />
-      <Icon src="/profile-inactive.svg" alt="profile-inactive" />
+      <Icon
+        src={`/movies-${router.pathname === "/" ? "active" : "inactive"}.svg`}
+        alt="movies-active"
+      />
+      <Icon
+        src={`/event-ticket-${
+          router.pathname === "/event-ticket" ? "active" : "inactive"
+        }.svg`}
+        alt="event-ticket-inactive"
+      />
+      <Icon
+        src={`/alarm-${
+          router.pathname === "/notifs" ? "active" : "inactive"
+        }.svg`}
+        alt="alarm-inactive"
+      />
+      <Icon
+        src={`/profile-${
+          router.pathname === "/profile" ? "active" : "inactive"
+        }.svg`}
+        alt="profile-inactive"
+      />
     </StyledContainer>
   );
 };
